7d7c8ad8298f9e5f7da0834953f3ac3609a7a031
Daniel Ruf <mac1@daniel-ruf.de>
2019-10-27 15:34:01 +0100

    Initial commit

---
+++ bin/patch-cutter.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+const patch_cutter = require('../index.js');
---
+++ index.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const readline = require('readline');
+const pkg = require('./package.json');
+
+console.log(`${pkg.name} ${pkg.version} (${pkg.author})${os.EOL}`);
+
+const patchFile = process.argv[2] || '';
+const validPatchFile = 
+  patchFile &&
+  fs.existsSync(patchFile) &&
+  patchFile.endsWith('.patch');
+
+if (!validPatchFile) {
+    console.log(`No patch file found.`);
+    process.exit(1);
+}
+
+const readInterface = readline.createInterface({
+    input: fs.createReadStream(patchFile),
+    output: null,
+    console: false
+});
+
+let patchNum = 0;
+let patchPath = '';
+let patchContent = [];
+
+readInterface.on('line', function (line) {
+    if (line.startsWith('diff --git')) {
+        if (patchNum) {
+            console.log(`Creating patch ${patchNum} (${patchPath})`);
+            fs.writeFileSync(patchPath, patchContent.join('\n'));
+            patchContent = [];
+        }
+        patchNum++;
+        const pathMatcher = /diff --git a\/(.+) b\/(.+)/;
+        const pathMatches = line.match(pathMatcher);
+        const dir = path.parse(pathMatches[1]).dir;
+        fs.mkdirSync(dir, { recursive: true });
+        patchPath = `${pathMatches[1]}.patch`;
+
+    } else {
+        if (patchNum) {
+            patchContent.push(line);
+        }
+    }
+});
+
+readInterface.on('close', function () {
+    if (patchNum) {
+        console.log(`Creating patch ${patchNum} (${patchPath})`);
+        fs.writeFileSync(patchPath, patchContent.join('\n'));
+        patchContent = [];
+    }
+});
